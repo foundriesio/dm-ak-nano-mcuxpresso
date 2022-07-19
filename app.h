3df43e5d5e7881edb24abe80919ffa8c7077199c
Andre Detsch <andre.detsch@foundries.io>
2022-07-19 17:17:19 -0300

    Update unchanged source and linker files from RT1064 to RT1060
    These files were copied from demo applications, and not changed
    afterwards
    
    Signed-off-by: Andre Detsch <andre.detsch@foundries.io>

--- app.h
+++ app.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2021 NXP
+ * Copyright 2020 NXP
  * All rights reserved.
  *
  *
@@ -12,15 +12,20 @@
  * Definitions
  ******************************************************************************/
 /*${macro:start}*/
-#define EXAMPLE_FLEXSPI                 FLEXSPI
-#define FLASH_SIZE                      0x2000 /* 64Mb/KByte */
-#define EXAMPLE_FLEXSPI_AMBA_BASE       FlexSPI_AMBA_BASE
-#define FLASH_PAGE_SIZE                 256
-#define EXAMPLE_SECTOR                  20
-#define SECTOR_SIZE                     0x1000 /* 4K */
-#define EXAMPLE_FLEXSPI_CLOCK           kCLOCK_FlexSpi
+#if !defined(XIP_EXTERNAL_FLASH) && !(XIP_EXTERNAL_FLASH > 1)
+#define USE_FLASH        1 /* Use FLASH */
+#define FLASH_ERASE_CHIP 0 /* Do not erase whole FlexSPI chip data */
+#endif                     /* XIP_EXTERNAL_FLASH */
+
+#if USE_FLASH
 #define FLASH_PORT                      kFLEXSPI_PortA1
-#define EXAMPLE_FLEXSPI_RX_SAMPLE_CLOCK kFLEXSPI_ReadSampleClkLoopbackFromDqsPad
+#define EXAMPLE_FLEXSPI           FLEXSPI1
+#define FLASH_SIZE                0x4000 /* 16Mb/KByte */
+#define EXAMPLE_FLEXSPI_AMBA_BASE FlexSPI1_AMBA_BASE
+#define FLASH_PAGE_SIZE           256
+#define EXAMPLE_SECTOR            20
+#define SECTOR_SIZE               0x1000 /* 4K */
+#define EXAMPLE_FLEXSPI_CLOCK     kCLOCK_Flexspi1
 
 #define NOR_CMD_LUT_SEQ_IDX_READ_NORMAL        7
 #define NOR_CMD_LUT_SEQ_IDX_READ_FAST          13
@@ -41,27 +46,14 @@
 #define FLASH_QUAD_ENABLE        0x40
 #define FLASH_BUSY_STATUS_POL    1
 #define FLASH_BUSY_STATUS_OFFSET 0
+#endif /* USE_FLASH */
 
-/*
- * If cache is enabled, this example should maintain the cache to make sure
- * CPU core accesses the memory, not cache only.
- */
-#define CACHE_MAINTAIN 1
-
+#define IEE IEE__IEE_RT1170 /* IEE defines */
 /*${macro:end}*/
 
-/*******************************************************************************
- * Variables
- ******************************************************************************/
-/*${variable:start}*/
-#if (defined CACHE_MAINTAIN) && (CACHE_MAINTAIN == 1)
-typedef struct _flexspi_cache_status
-{
-    volatile bool DCacheEnableFlag;
-    volatile bool ICacheEnableFlag;
-} flexspi_cache_status_t;
-#endif
-/*${variable:end}*/
+/* Note: region is [bot:top), the end is open interval. So the bit[2:0] of the end address must be zero. */
+#define StartAddr 0x20360000
+#define EndAddr   0x2036FFF8
 
 /*******************************************************************************
  * Prototypes
@@ -70,16 +62,9 @@ typedef struct _flexspi_cache_status
 void BOARD_InitHardware(void);
 static inline void flexspi_clock_init(void)
 {
-#if defined(XIP_EXTERNAL_FLASH) && (XIP_EXTERNAL_FLASH == 1)
-    CLOCK_SetMux(kCLOCK_FlexspiMux, 0x2); /* Choose PLL2 PFD2 clock as flexspi source clock. 396M */
-    CLOCK_SetDiv(kCLOCK_FlexspiDiv, 2);   /* flexspi clock 133M. */
-#else
-    const clock_usb_pll_config_t g_ccmConfigUsbPll = {.loopDivider = 0U};
-    CLOCK_InitUsb1Pll(&g_ccmConfigUsbPll);
-    CLOCK_InitUsb1Pfd(kCLOCK_Pfd0, 24);   /* Set PLL3 PFD0 clock 360MHZ. */
-    CLOCK_SetMux(kCLOCK_FlexspiMux, 0x3); /* Choose PLL3 PFD0 clock as flexspi source clock. */
-    CLOCK_SetDiv(kCLOCK_FlexspiDiv, 2);   /* flexspi clock 120M. */
-#endif
+    /*Clock setting for flexspi1*/
+    CLOCK_SetRootClockDiv(kCLOCK_Root_Flexspi1, 2);
+    CLOCK_SetRootClockMux(kCLOCK_Root_Flexspi1, 0);
 }
 /*${prototype:end}*/
 
