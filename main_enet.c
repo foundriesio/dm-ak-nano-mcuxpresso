cb3d1e963e47b710323a9569a32f3a114c654877
Andre Detsch <andre.detsch@foundries.io>
2022-07-19 17:19:04 -0300

    Adjust main application initialization for RT1170
    Signed-off-by: Andre Detsch <andre.detsch@foundries.io>

--- main_enet.c
+++ main_enet.c
@@ -46,6 +46,15 @@
 #include "types/iot_network_types.h"
 #include "aws_demo.h"
 
+#if BOARD_NETWORK_USE_100M_ENET_PORT
+#include "fsl_phyksz8081.h"
+#else
+#include "fsl_phyrtl8211f.h"
+#endif
+#include "fsl_enet_mdio.h"
+#include "fsl_gpio.h"
+#include "fsl_iomuxc.h"
+#include "fsl_enet.h"
 #include "fsl_phy.h"
 /* lwIP Includes */
 #include "lwip/tcpip.h"
@@ -54,20 +63,9 @@
 #include "netif/ethernet.h"
 #include "enet_ethernetif.h"
 #include "lwip/netifapi.h"
-#include "fsl_phyksz8081.h"
-#include "fsl_enet_mdio.h"
-#include "fsl_gpio.h"
-#include "fsl_iomuxc.h"
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
-#ifndef EXAMPLE_NETIF_INIT_FN
-/*! @brief Network interface initialization function. */
-#define EXAMPLE_NETIF_INIT_FN ethernetif0_init
-#endif /* EXAMPLE_NETIF_INIT_FN */
-
-#define INIT_SUCCESS 0
-#define INIT_FAIL    1
 
 /* MAC address configuration. */
 #define configMAC_ADDR                     \
@@ -75,17 +73,35 @@
         0x02, 0x12, 0x13, 0x10, 0x15, 0x25 \
     }
 
+#if BOARD_NETWORK_USE_100M_ENET_PORT
 /* Address of PHY interface. */
 #define EXAMPLE_PHY_ADDRESS BOARD_ENET0_PHY_ADDRESS
+/* PHY operations. */
+#define EXAMPLE_PHY_OPS phyksz8081_ops
+/* ENET instance select. */
+#define EXAMPLE_NETIF_INIT_FN ethernetif0_init
+#else
+/* Address of PHY interface. */
+#define EXAMPLE_PHY_ADDRESS   BOARD_ENET1_PHY_ADDRESS
+/* PHY operations. */
+#define EXAMPLE_PHY_OPS       phyrtl8211f_ops
+/* ENET instance select. */
+#define EXAMPLE_NETIF_INIT_FN ethernetif1_init
+#endif
 
 /* MDIO operations. */
 #define EXAMPLE_MDIO_OPS enet_ops
 
-/* PHY operations. */
-#define EXAMPLE_PHY_OPS phyksz8081_ops
-
 /* ENET clock frequency. */
-#define EXAMPLE_CLOCK_FREQ CLOCK_GetFreq(kCLOCK_IpgClk)
+#define EXAMPLE_CLOCK_FREQ CLOCK_GetRootClockFreq(kCLOCK_Root_Bus)
+
+#ifndef EXAMPLE_NETIF_INIT_FN
+/*! @brief Network interface initialization function. */
+#define EXAMPLE_NETIF_INIT_FN ethernetif0_init
+#endif /* EXAMPLE_NETIF_INIT_FN */
+
+#define INIT_SUCCESS 0
+#define INIT_FAIL    1
 
 /* @TEST_ANCHOR */
 
@@ -117,6 +133,47 @@ struct netif netif;
 /*******************************************************************************
  * Code
  ******************************************************************************/
+void BOARD_InitModuleClock(void)
+{
+    const clock_sys_pll1_config_t sysPll1Config = {
+        .pllDiv2En = true,
+    };
+    CLOCK_InitSysPll1(&sysPll1Config);
+
+#if BOARD_NETWORK_USE_100M_ENET_PORT
+    clock_root_config_t rootCfg = {.mux = 4, .div = 10}; /* Generate 50M root clock. */
+    CLOCK_SetRootClock(kCLOCK_Root_Enet1, &rootCfg);
+#else
+    clock_root_config_t rootCfg = {.mux = 4, .div = 4}; /* Generate 125M root clock. */
+    CLOCK_SetRootClock(kCLOCK_Root_Enet2, &rootCfg);
+#endif
+
+    /* Select syspll2pfd3, 528*18/24 = 396M */
+    CLOCK_InitPfd(kCLOCK_PllSys2, kCLOCK_Pfd3, 24);
+    rootCfg.mux = 7;
+    rootCfg.div = 2;
+    CLOCK_SetRootClock(kCLOCK_Root_Bus, &rootCfg); /* Generate 198M bus clock. */
+}
+
+void IOMUXC_SelectENETClock(void)
+{
+#if BOARD_NETWORK_USE_100M_ENET_PORT
+    IOMUXC_GPR->GPR4 |= 0x3; /* 50M ENET_REF_CLOCK output to PHY and ENET module. */
+#else
+    IOMUXC_GPR->GPR5 |= IOMUXC_GPR_GPR5_ENET1G_RGMII_EN_MASK; /* bit1:iomuxc_gpr_enet_clk_dir
+                                                                 bit0:GPR_ENET_TX_CLK_SEL(internal or OSC) */
+#endif
+}
+
+void BOARD_ENETFlexibleConfigure(enet_config_t *config)
+{
+#if BOARD_NETWORK_USE_100M_ENET_PORT
+    config->miiMode = kENET_RmiiMode;
+#else
+    config->miiMode = kENET_RgmiiMode;
+#endif
+}
+
 int initNetwork(void)
 {
     ip4_addr_t netif_ipaddr, netif_netmask, netif_gw;
@@ -159,25 +216,6 @@ int initNetwork(void)
 
     return INIT_SUCCESS;
 }
-void BOARD_InitModuleClock(void)
-{
-    const clock_enet_pll_config_t config = {
-        .enableClkOutput    = true,
-        .enableClkOutput25M = false,
-        .loopDivider        = 1,
-    };
-    CLOCK_InitEnetPll(&config);
-}
-
-void delay(void)
-{
-    volatile uint32_t i = 0;
-    for (i = 0; i < 1000000; ++i)
-    {
-        __asm("NOP"); /* delay */
-    }
-}
-
 
 void print_string(const char *string)
 {
@@ -239,20 +277,39 @@ int main(void)
     gpio_pin_config_t gpio_config = {kGPIO_DigitalOutput, 0, kGPIO_NoIntmode};
 
     BOARD_ConfigMPU();
-    BOARD_InitBootPins();
-    BOARD_InitBootClocks();
+    BOARD_InitPins();
+    BOARD_BootClockRUN();
     BOARD_InitDebugConsole();
     BOARD_InitModuleClock();
 
-    IOMUXC_EnableMode(IOMUXC_GPR, kIOMUXC_GPR_ENET1TxClkOutputDir, true);
+    SCB_DisableDCache();
 
-    GPIO_PinInit(GPIO1, 9, &gpio_config);
-    GPIO_PinInit(GPIO1, 10, &gpio_config);
-    /* pull up the ENET_INT before RESET. */
-    GPIO_WritePinOutput(GPIO1, 10, 1);
-    GPIO_WritePinOutput(GPIO1, 9, 0);
-    delay();
-    GPIO_WritePinOutput(GPIO1, 9, 1);
+    IOMUXC_SelectENETClock();
+
+#if BOARD_NETWORK_USE_100M_ENET_PORT
+xx
+    BOARD_InitEnetPins();
+    GPIO_PinInit(GPIO9, 11, &gpio_config);
+    GPIO_PinInit(GPIO12, 12, &gpio_config);
+    /* Pull up the ENET_INT before RESET. */
+    GPIO_WritePinOutput(GPIO9, 11, 1);
+    GPIO_WritePinOutput(GPIO12, 12, 0);
+    SDK_DelayAtLeastUs(10000, CLOCK_GetFreq(kCLOCK_CpuClk));
+    GPIO_WritePinOutput(GPIO12, 12, 1);
+    SDK_DelayAtLeastUs(6, CLOCK_GetFreq(kCLOCK_CpuClk));
+#else
+    BOARD_InitEnet1GPins();
+    GPIO_PinInit(GPIO11, 14, &gpio_config);
+    /* For a complete PHY reset of RTL8211FDI-CG, this pin must be asserted low for at least 10ms. And
+     * wait for a further 30ms(for internal circuits settling time) before accessing the PHY register */
+    GPIO_WritePinOutput(GPIO11, 14, 0);
+    SDK_DelayAtLeastUs(10000, CLOCK_GetFreq(kCLOCK_CpuClk));
+    GPIO_WritePinOutput(GPIO11, 14, 1);
+    SDK_DelayAtLeastUs(30000, CLOCK_GetFreq(kCLOCK_CpuClk));
+
+    EnableIRQ(ENET_1G_MAC0_Tx_Rx_1_IRQn);
+    EnableIRQ(ENET_1G_MAC0_Tx_Rx_2_IRQn);
+#endif
     CRYPTO_InitHardware();
 
     xLoggingTaskInitialize(LOGGING_TASK_STACK_SIZE, LOGGING_TASK_PRIORITY, LOGGING_QUEUE_LENGTH);
